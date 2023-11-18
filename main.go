17d0a38560d3a4639a104ae15d749bdef8415d47
Vojtch159 <vojta159.studeny@gmail.com>
2023-11-18 16:46:48 +0300

    add poc blob merger

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+const (
+	MAX_BLOB_SIZE_IN_BYTES = 1024 * 128
+)
 
-func main() {
-    fmt.Println("Unicorn.")
+func Merge_blob_data(blobs ...[]byte) [][]byte {
+	var result [][]byte
+	var blob []byte
+	result = append(result, blob)
+	for _, blob_data := range blobs {
+		var flag bool = false
+		if len(blob_data) > MAX_BLOB_SIZE_IN_BYTES {
+			continue
+		}
+		for i, blob := range result {
+			if len(blob)+len(blob_data) <= MAX_BLOB_SIZE_IN_BYTES {
+				flag = true
+				result[i] = append(blob, blob_data...)
+				break
+			}
+		}
+		if !flag {
+			var blob []byte
+			copy(blob, blob_data)
+			result = append(result, blob)
+		}
+	}
+	return result
 }
